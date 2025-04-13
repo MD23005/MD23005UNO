d25254bfad767d2824f9d5cf54aa5f4e0d830410
Ronald Manzano <md23005@ues.edu.sv>
2025-04-12 21:48:42 -0600

    Subiendo archivos fuente

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+setup(
+    name='MD23005UNO',
+    version='0.1',
+    packages=['MD23005UNO'],
+    description='Librería que contiene diferentes métodos para resolver sistemas de ecuaciones lineales',
+    long_description=open('README.md',encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    author='Ronald Manzano',
+    author_email='md23005@ues.edu.sv',
+    url=
+    
+)
